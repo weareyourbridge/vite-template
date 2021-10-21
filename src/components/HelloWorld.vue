b21777188d6da1537ed05918be29eafed3a47848
Niek van Sleeuwen <78557728+niekbridge@users.noreply.github.com>
2021-10-21 10:12:02 +0100

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="count++">count is: {{ count }}</button>
+</template>
